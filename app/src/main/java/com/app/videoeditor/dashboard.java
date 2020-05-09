f50182622a4d0151ffdcd9df745994af9f6fedd5
singhvedant111 <31094075+singhvedant111@users.noreply.github.com>
2020-05-09 13:29:34 +0530

    ADDED CHUTIYAPA

--- app/src/main/java/com/app/videoeditor/dashboard.java
+++ app/src/main/java/com/app/videoeditor/dashboard.java
@@ -21,6 +21,8 @@ import com.google.firebase.storage.StorageReference;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 public class dashboard extends AppCompatActivity {
@@ -29,6 +31,24 @@ public class dashboard extends AppCompatActivity {
     private StorageReference storageReference;
     private FirebaseDatabase firebaseDatabase;
 
+
+   private String getproductid(String uid,String uniqueid) throws NoSuchAlgorithmException {
+        String stringToEncrypt = uid;
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.update(stringToEncrypt.getBytes());
+        messageDigest.update(uniqueid.getBytes());
+        byte[] digest = messageDigest.digest();
+        System.out.println(digest);
+
+        //Converting the byte array in to HexString format
+        StringBuffer hexString = new StringBuffer();
+
+        for (int i = 0;i<digest.length;i++) {
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+        }
+        return ("Hex format : " + hexString.toString());
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +59,7 @@ public class dashboard extends AppCompatActivity {
         if (userd != null) {
             assert user != null;
             databaseReference = firebaseDatabase.getReference("users/" + Objects.requireNonNull(user.getUid()));
-            
+
 
 
         }
